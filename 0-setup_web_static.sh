be3c3247fb8d8cf30341d6cb5d61b2f3a2acf777
Ayivi Angela <104152275+bloominghowl@users.noreply.github.com>
2023-07-06 09:26:21 +0000

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,30 +1,48 @@
 #!/usr/bin/env bash
-# Sets up a web server for deployment of web_static.
+# sets up my web servers for the deployment of web_static
 
-if ! command -v nginx &> /dev/null; then
-    apt-get -y update
-    apt-get -y install nginx
-fi
+echo -e "\e[1;32m START\e[0m"
 
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
+#--Updating the packages
+sudo apt-get -y update
+sudo apt-get -y install nginx
+echo -e "\e[1;32m Packages updated\e[0m"
+echo
 
-echo "<html>
-  <head>
-  </head>
-  <body>
-    <p>Test HTML file for Nginx</p>
-  </body>
-</html>" > /data/web_static/releases/test/index.html
+#--configure firewall
+sudo ufw allow 'Nginx HTTP'
+echo -e "\e[1;32m Allow incomming NGINX HTTP connections\e[0m"
+echo
 
-if [ -L /data/web_static/current ]; then
-    rm /data/web_static/current
-fi
-ln -sf /data/web_static/releases/test/ /data/web_static/current
+#--created the dir
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
+echo -e "\e[1;32m directories created"
+echo
 
-chown -R ubuntu:ubuntu /data/
+#--adds test string
+echo "<h1>Welcome</h1>" > /data/web_static/releases/test/index.html
+echo -e "\e[1;32m Test string added\e[0m"
+echo
 
-config_file="/etc/nginx/sites-available/default"
-sed -i '/^\tserver_name localhost;/a \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n' $config_file
+#--prevent overwrite
+if [ -d "/data/web_static/current" ];
+then
+    echo "path /data/web_static/current exists"
+    sudo rm -rf /data/web_static/current;
+fi;
+echo -e "\e[1;32m prevent overwrite\e[0m"
+echo
 
-service nginx restart
+#--create symbolic link
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -hR ubuntu:ubuntu /data
+
+sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+
+sudo ln -sf '/etc/nginx/sites-available/default' '/etc/nginx/sites-enabled/default'
+echo -e "\e[1;32m Symbolic link created\e[0m"
+echo
+
+#--restart NGINX
+sudo service nginx restart
+echo -e "\e[1;32m restart NGINX\e[0m"
